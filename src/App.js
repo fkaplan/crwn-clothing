a1ccee1b8917ba63c54a2dc232118f7e08111738
DESKTOP-84M71MF\furkan <furkankaplan@gmail.com>
2020-04-05 20:10:35 +0300

    header componenti eklenerek  homepage, shop page ve contact linkleri eklendi.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component.jsx';
 import "./App.css";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
-        <Route path="/hats" component={HatsPage}></Route>
+        <Route path="/shop" component={ShopPage}></Route>
       </Switch>
     </div>
   );
